c2989ff8f360f0895737728ef7d3e36c4d2b10eb
Debopriyo <debopriyo.ghosh@globalids.com>
2024-06-21 15:42:56 +0530

    Changes on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,86 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app color="primary" dark>
+      <h3>My Table</h3>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-row class="mt-2">
+          <v-text-field class="mr-2" v-model="serverIp" label="Enter Server IP here" outlined clearable id="serverUrl">
+          </v-text-field>
+          <v-text-field v-model="token" label="Enter Bearer token here" outlined clearable id="apiToken"></v-text-field>
+        </v-row>
+        <v-row align="center" justify="space-around">
+          <!-- <v-btn @click="ShowTable(showData)">{{ !showData ? "Fetch Data" : "Reset" }}</v-btn> demo data to show table-->
+          <v-btn @click="GetTables()">{{ "Fetch Data" }}</v-btn>
+        </v-row>
+        <v-card class="mt-8">
+          <v-card-title>
+            <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line
+              hide-details></v-text-field>
+          </v-card-title>
+          <MyTable :headers="headers" :items="tables" :search="search" />
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import MyTable from './components/MyTable';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    MyTable,
   },
+  data() {
+    return {
+      search: '',
+      showData: false,
+      serverIp: "",
+      token: "",
+      tables: [],
+      headers: [
+        {
+          text: 'Name', align: 'start', value: 'label',
+        },
+        { text: 'Schema', value: 'schema.label' },
+        { text: 'Database', value: 'database.label' },
+        { text: 'Application', value: 'application.label' },
+        { text: 'Tags', value: 'tagValues.label' },
+        { text: 'Columns', value: 'columnsTotal' },
+        { text: 'Records', value: 'columnsTotal' },
+        { text: 'Profilable Columns', value: 'columnsTotal' },
+        { text: 'Profiled Columns', value: 'columnsTotal' },
+        { text: 'Classified Columns', value: 'columnsTotal' },
+      ],
+    }
+  },
+  methods: {
+    ShowTable(v) {
+      this.showData = !v
+    },
+    async GetTables() {
+      console.log(this.token);
+      console.log(this.serverIp);
+      try {
+        const response = await fetch(
+          this.serverIp + '/api/v2.1/tables?limit=100&token=' + this.token
+        );
+        const tablesData = await response.json();
+        this.tables = tablesData.tables;
+        console.log(tablesData);
+      } catch (error) {
+        console.error(error);
+      }
 
-  data: () => ({
-    //
-  }),
+    }
+  }
 };
+
+
+
 </script>
